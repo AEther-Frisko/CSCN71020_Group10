649aa8ece24b50fc429ac97bf75879b9cbdd2a78
max <avenables1135@conestogac.on.ca>
2022-11-14 14:35:56 -0500

    Added triangle angle calculation

--- PolygonChecker/triangleSolver.c
+++ PolygonChecker/triangleSolver.c
@@ -1,5 +1,8 @@
+#define _USE_MATH_DEFINES
+
 #include <stdio.h>
 #include <stdbool.h>
+#include <math.h>
 
 #include "triangleSolver.h"
 
@@ -19,6 +22,26 @@ char* analyzeTriangle(int side1, int side2, int side3) {
 	else {
 		result = "Scalene triangle";
 	}
+	if (result != "Not a triangle"){
+		GetTriangleAngles(side1, side2, side3);
+	}
 
 	return result;
+}
+
+void GetTriangleAngles(int side1, int side2, int side3){
+	double angle1 = ((side2 * side2) + (side3 * side3) - (side1 * side1));
+	angle1 = angle1 / (2 * side2 * side3);
+	angle1 = acos(angle1);
+	angle1 = angle1 * (180 / M_PI);
+
+	double angle2 = ((side1 * side1) + (side3 * side3) - (side2 * side2));
+	angle2 = angle2 / (2 * side1 * side3);
+	angle2 = acos(angle2);
+	angle2 = angle2 * (180 / M_PI);
+	
+	double angle3 = 180 - angle1 - angle2;
+
+
+	printf("The angles of this triangle are: %lf, %lf, %lf\n", angle1, angle2, angle3);
 }
